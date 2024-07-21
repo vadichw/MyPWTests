44f4f5bc85a99f190f8f4e54d522c43bb1260dac
vadichw <vadik56780098@gmail.com>
2024-07-21 16:26:04 +0300

    add

--- MyPWTests/UnitTest1.cs
+++ MyPWTests/UnitTest1.cs
@@ -48,6 +48,8 @@ namespace MyPWTests
             else
             {
                 Console.WriteLine($"It's the incorrect title. Found title is {actualTitle}");
+                Console.WriteLine($"It's the incorrect title. Found title is {actualTitle}");
+
             }
         }
     }
